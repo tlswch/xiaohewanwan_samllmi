e2e78e164ec63f93b415ec9bd6325d69f48494e9
small mi <1225339345@qq.com>
2022-12-20 02:15:37 +0000

    更新 'js/yunjing.js'

--- js/yunjing.js
+++ js/yunjing.js
@@ -30,8 +30,8 @@ var rule = {
 		"img":".detail-pic&&data-original",
 		"desc":".fraction&&Text;.slide-info-remarks:eq(1)&&Text;.slide-info-remarks:eq(2)&&Text;.slide-info:eq(2)--strong&&Text;.slide-info:eq(1)--strong&&Text",
 		"content":"#height_limit&&Text",
-		"tabs":".anthology-tab.nav-swiper.b-b.br&&.swiper-wrapper",
-		"tab_text":".fa&&Text",
+		"tabs":".anthology-tab&&a",
+		"tab_text":"Text",
 		"lists":".anthology-list-box:eq(#id) li"
 	},
     搜索:'json:list;name;pic;;id',
