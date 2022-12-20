8fa9315b21c19f1a34acb237582d74e8c85047ac
small mi <1225339345@qq.com>
2022-12-20 06:46:44 +0000

    更新 'js/aidanmu.js'

--- js/aidanmu.js
+++ js/aidanmu.js
@@ -27,7 +27,7 @@ var rule = {
     一级:'js:let body=input.split("#")[1];let t=Math.round(new Date/1e3).toString();let key=md5("DS"+t+"DCC147D11943AF75");let url=input.split("#")[0];body=body+"&time="+t+"&key="+key;print(body);fetch_params.body=body;let html=post(url,fetch_params);let data=JSON.parse(html);VODS=data.list.map(function(it){it.vod_pic=it.vod_pic.replace("mac:","https:");return it});',
     二级:{
 		"title":".slide-info-title&&Text;.slide-info:eq(3)--strong&&Text",
-		"img":"a&&data-original",
+		"img":"body&&.wow.fadeInLeft;a&&data-original",
 		"desc":".fraction&&Text;.gen-search-form.search-show&&li;li:eq(1)&&Text;li:eq(2)&&Text;.slide-info:eq(2)--strong&&Text;.slide-info:eq(1)--strong&&Text",
 		"content":"#height_limit&&Text",
 		"tabs":".anthology.wow.fadeInUp.animated&&.swiper-wrapper&&a",
