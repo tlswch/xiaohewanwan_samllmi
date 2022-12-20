36701664038541243b3c2c168f31f144948ee400
small mi <1225339345@qq.com>
2022-12-20 03:15:35 +0000

    更新 'js/yunjing.js'

--- js/yunjing.js
+++ js/yunjing.js
@@ -32,7 +32,7 @@ var rule = {
 		"content":"#height_limit&&Text",
 		"tabs":".anthology.wow.fadeInUp.animated&&.swiper-wrapper",
 		"tab_text":"a&&Text",
-		"lists":".anthology-list-play&&ul&&li"
+		"lists":"body&&.anthology-list-play&&li"
 	},
     搜索:'json:list;name;pic;;id',
 }
