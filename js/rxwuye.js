ba1c70aa5b04e0f5604789e1785a11f7302efe11
small mi <1225339345@qq.com>
2023-01-08 10:29:54 +0000

    更新 'js/rxwuye.js'

--- js/rxwuye.js
+++ js/rxwuye.js
@@ -26,7 +26,7 @@ var rule = {
     一级:'js:let body=input.split("#")[1];let t=Math.round(new Date/1e3).toString();let key=md5("DS"+t+"DCC147D11943AF75");let url=input.split("#")[0];body=body+"&time="+t+"&key="+key;print(body);fetch_params.body=body;let html=post(url,fetch_params);let data=JSON.parse(html);VODS=data.list;',
     二级:{
 		"title":".slide-info-title&&Text;.slide-info:eq(3)--strong&&Text",
-		"img":".detail-pic&&data-original",
+		"img":".lazy&&data-original",
 		"desc":".fraction&&Text;.slide-info-remarks:eq(1)&&Text;.slide-info-remarks:eq(2)&&Text;.slide-info:eq(2)--strong&&Text;.slide-info:eq(1)--strong&&Text",
 		"content":"#height_limit&&Text",
 		"tabs":".anthology.wow.fadeInUp.animated&&.swiper-wrapper&&a",
