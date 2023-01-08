1f1fafd53a325d634a21c3dd0535098c0db63cab
small mi <1225339345@qq.com>
2023-01-08 14:41:33 +0000

    更新 'js/dadou.js'

--- js/dadou.js
+++ js/dadou.js
@@ -21,13 +21,13 @@ var rule = {
 		4:{cateId:'4'}
 	},
     detailUrl:'/Vod/fyid.html',
-	图片来源:'@Referer=https://dadou.pro',
-    推荐:'.list-vod.flex .public-list-box;a&&title;a&&data-original;.public-list-prb&&Text;a&&href',
+	图片来源:'@Referer=https://dadou.pro/',
+    推荐:'.list-vod.flex .public-list-box;a&&title;.lazy&&data-original;.public-list-prb&&Text;a&&href',
     一级:'',
     一级:'js:let body=input.split("#")[1];let t=Math.round(new Date/1e3).toString();let key=md5("DS"+t+"DCC147D11943AF75");let url=input.split("#")[0];body=body+"&time="+t+"&key="+key;print(body);fetch_params.body=body;let html=post(url,fetch_params);let data=JSON.parse(html);VODS=data.list;',
     二级:{
 		"title":".slide-info-title&&Text;.slide-info:eq(3)--strong&&Text",
-		"img":".detail-pic.lazy.mask-1&&data-original",
+		"img":".detail-pic&&data-original",
 		"desc":".fraction&&Text;.slide-info-remarks:eq(1)&&Text;.slide-info-remarks:eq(2)&&Text;.slide-info:eq(2)--strong&&Text;.slide-info:eq(1)--strong&&Text",
 		"content":"#height_limit&&Text",
 		"tabs":".anthology.wow.fadeInUp.animated&&.swiper-wrapper&&a",
