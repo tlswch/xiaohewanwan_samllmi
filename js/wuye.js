be8c1d1451ad951d4a109539be05054e555e1ab0
small mi <1225339345@qq.com>
2023-01-08 11:08:28 +0000

    更新 'js/wuye.js'

--- js/wuye.js
+++ js/wuye.js
@@ -7,7 +7,7 @@ var rule = {
     searchUrl: '/index.php/ajax/suggest?mid=1&wd=**',
     searchable: 2,
     quickSearch: 0,
-    headers:{'User-Agent':'MOBILE_UA'},
+    headers:{'User-Agent':'Mozilla/5.0 (Linux; Android 12; 22041211AC Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/96.0.4664.104 Mobile Safari/537.36'},
     // 分类链接fypage参数支持1个()表达式
     // url: '/index.php/api/vod#type=fyclass&page=fypage',
 	url: '/index.php/api/vod#type=fyfilter&page=fypage',
@@ -21,7 +21,7 @@ var rule = {
 		43:{cateId:'43'}
 	},
     detailUrl:'/index.php/vod/detail/id/fyid.html',
-    图片来源:'@Referer=http://rxwuye.com',
+    图片来源:'@Referer=rxwuye.com',
     推荐:'.list-vod.flex .public-list-box;a&&title;.lazy&&data-original;.public-list-prb&&Text;a&&href',
     一级:'',
     一级:'js:let body=input.split("#")[1];let t=Math.round(new Date/1e3).toString();let key=md5("DS"+t+"DCC147D11943AF75");let url=input.split("#")[0];body=body+"&time="+t+"&key="+key;print(body);fetch_params.body=body;let html=post(url,fetch_params);let data=JSON.parse(html);VODS=data.list.map(function(it){it.vod_pic=it.vod_pic.replace("mac:","https:");return it});',
