98694abdb22c6bfccf48bdd2164648b67da54312
small mi <1225339345@qq.com>
2023-01-08 10:41:11 +0000

    更新 'js/rxwuye.js'

--- js/rxwuye.js
+++ js/rxwuye.js
@@ -7,8 +7,7 @@ var rule = {
     searchUrl: '/index.php/ajax/suggest?mid=1&wd=**',
     searchable: 2,
     quickSearch: 0,
-    headers:{'User-Agent':'PC_UA'},
-	图片来源:'@Referer=http://rxwuye.com/',
+    headers:{'User-Agent':'MOBILE_UA'},
     // 分类链接fypage参数支持1个()表达式
     // url: '/index.php/api/vod#type=fyclass&page=fypage',
 	url: '/index.php/api/vod#type=fyfilter&page=fypage',
@@ -22,12 +21,13 @@ var rule = {
 		4:{cateId:'4'}
 	},
     detailUrl:'/index.php/vod/detail/id/fyid.html',
+	图片来源:'@Referer=http://rxwuye.com/',
     推荐:'.list-vod.flex .public-list-box;a&&title;.lazy&&data-original;.public-list-prb&&Text;a&&href',
     一级:'',
     一级:'js:let body=input.split("#")[1];let t=Math.round(new Date/1e3).toString();let key=md5("DS"+t+"DCC147D11943AF75");let url=input.split("#")[0];body=body+"&time="+t+"&key="+key;print(body);fetch_params.body=body;let html=post(url,fetch_params);let data=JSON.parse(html);VODS=data.list;',
     二级:{
 		"title":".slide-info-title&&Text;.slide-info:eq(3)--strong&&Text",
-		"img":".lazy&&data-original",
+		"img":".detail-pic&&data-original",
 		"desc":".fraction&&Text;.slide-info-remarks:eq(1)&&Text;.slide-info-remarks:eq(2)&&Text;.slide-info:eq(2)--strong&&Text;.slide-info:eq(1)--strong&&Text",
 		"content":"#height_limit&&Text",
 		"tabs":".anthology.wow.fadeInUp.animated&&.swiper-wrapper&&a",
