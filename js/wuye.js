122c39db081ada9dc3be7a4d96395de8a38e1014
small mi <1225339345@qq.com>
2023-01-27 05:10:08 +0000

    更新 'js/wuye.js'

--- js/wuye.js
+++ js/wuye.js
@@ -10,7 +10,7 @@ var rule = {
     headers:{'User-Agent':'Mozilla/5.0 (Linux; Android 12; 22041211AC Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/96.0.4664.104 Mobile Safari/537.36'},
     // 分类链接fypage参数支持1个()表达式
     // url: '/index.php/api/vod#type=fyclass&page=fypage',
-	url: '/index.php/api/vod#type=fyfilter&page=fypage',
+	url: '/index.php/api/vod#type=fyclass&page=fypage',
 	filterable:1,//是否启用分类筛选,
 	filter_url:'{{fl.cateId}}',
 	filter: {"1":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"1"},{"n":"动作片","v":"6"},{"n":"喜剧片","v":"7"},{"n":"爱情片","v":"8"},{"n":"科幻片","v":"9"},{"n":"恐怖片","v":"10"},{"n":"剧情片","v":"11"},{"n":"战争片","v":"12"},{"n":"记录片","v":"13"},{"n":"动画","v":"14"},{"n":"1080电影","v":"46"},{"n":"其他片","v":"36"}]}],"2":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"2"},{"n":"国产剧","v":"13"},{"n":"港剧","v":"14"},{"n":"欧美剧","v":"16"},{"n":"日韩剧","v":"15"},{"n":"高清剧","v":"50"}]}]},
