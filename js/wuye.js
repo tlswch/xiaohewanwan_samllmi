c3b39657ddb049552f4ec0e241a481a986c13900
small mi <1225339345@qq.com>
2023-01-27 05:18:07 +0000

    更新 'js/wuye.js'

--- js/wuye.js
+++ js/wuye.js
@@ -1,6 +1,6 @@
 var rule = {
     title: '午夜影视',
-    host: 'http://rxwuye.com',
+    host: 'https://rxwuye.com',
     class_name:'电影&电视剧&综艺&动漫',
     class_url:'1&2&3&4',
     homeUrl: 'https://rxwuye.com',
@@ -11,13 +11,7 @@ var rule = {
     // 分类链接fypage参数支持1个()表达式
     // url: '/index.php/api/vod#type=fyclass&page=fypage',
 	url: '/index.php/api/vod#type=fyclass&page=fypage',
-	filterable:0,//是否启用分类筛选,
-	filter_url:'{{fl.cateId}}',
-	filter: {"1":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"1"},{"n":"动作片","v":"6"},{"n":"喜剧片","v":"7"},{"n":"爱情片","v":"8"},{"n":"科幻片","v":"9"},{"n":"恐怖片","v":"10"},{"n":"剧情片","v":"11"},{"n":"战争片","v":"12"},{"n":"记录片","v":"13"},{"n":"动画","v":"14"},{"n":"1080电影","v":"46"},{"n":"其他片","v":"36"}]}],"2":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"2"},{"n":"国产剧","v":"13"},{"n":"港剧","v":"14"},{"n":"欧美剧","v":"16"},{"n":"日韩剧","v":"15"},{"n":"高清剧","v":"50"}]}]},
-    filter_def:{
-		20:{cateId:'1'},
-		37:{cateId:'2'}
-	},
+	
     detailUrl:'/index.php/vod/detail/id/fyid.html',
     图片来源:'@Referer=http://rxwuye.com/',
     推荐:'.list-vod.flex .public-list-box;a&&title;.lazy&&data-original;.public-list-prb&&Text;a&&href',
